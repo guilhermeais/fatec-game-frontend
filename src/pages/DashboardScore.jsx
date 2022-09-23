6a5668d1dfa2172894717d5196ea6086320ee311
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-09-23 08:22:08 -0300

    feat: utiliza PlayersCard na DashboardScore

--- src/pages/DashboardScore.jsx
+++ src/pages/DashboardScore.jsx
@@ -1,22 +1,52 @@
-import { Stack, Typography } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { PlayersCard } from '../components/PlayersCard'
+import { playersScore } from '../services/playersScore'
+
 import '../styles/dashboard-score.scss'
-import {  EnhancedTable } from '../components/EnhancedTable'
+
+function getPlayersCard(players = []) {
+  return players.map((player, i) => (
+    <PlayersCard
+      key={`${player.playerName} - ${i}`}
+      player={{
+        name: player.playerName,
+        score: player.score,
+      }}
+    />
+  ))
+}
+
 export function DashboardScore() {
+  const [players, setPlayers] = useState([])
+
+  async function getPlayersScore({ playerName } = {}) {
+    if (playerName) {
+      const players = await playersScore.getPlayersScore({ playerName })
+      setPlayers(players)
+    } else {
+      const players = await playersScore.getPlayersScore({ playerName })
+      setPlayers(players)
+    }
+  }
+
+  useEffect(() => {
+    getPlayersScore()
+  }, [])
   return (
     <div>
-      <Stack spacing={6}>
-        <Typography
-        variant="h2"
-        sx={{
-          textAlign: 'center',
-          fontWeight: 500,
-        }}
+      <Typography
+        variant="h4"
+        component="h1"
+        sx={{ textAlign: 'center', mt: 5, mb: 5 }}
       >
-        Ranking dos Jogadores
+        <b>Ranking dos Jogares</b>
       </Typography>
-
-      <EnhancedTable></EnhancedTable>
-      </Stack>
+      <Box sx={{ width: '100%', margin: '1rem' }}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          {getPlayersCard(players)}
+        </Grid>
+      </Box>
     </div>
   )
 }
