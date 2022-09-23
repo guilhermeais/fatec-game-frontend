0a8b8af6ea14ef40171802583563362f7141ca39
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-09-23 08:20:55 -0300

    feat: cria playersScore

--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import { Axios } from 'axios'
-
-const apiService = new Axios({
-  baseURL: process.env.REACT_APP_API_SERVICE || 'http://localhost:3000'
-})
-
-export default apiService
---
+++ src/services/playersScore.js
@@ -0,0 +1,11 @@
+const baseUrl = process.env.REACT_APP_API_SERVICE || 'localhost:3001';
+export async function getPlayersScore({playerName} = {}) {
+  if (playerName) {
+    const players =  (await fetch(`${baseUrl}/players/${playerName}/score`, {method: 'GET'})).json()
+    return players
+  } else {
+    const players = (await fetch(`${baseUrl}/players/score`, {method: 'GET'})).json()
+
+    return players
+  }
+}
